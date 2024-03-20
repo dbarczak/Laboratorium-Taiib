d257e21b7ae4907513faf223ddd234b16361e3e0
dbarczak <92883028+dbarczak@users.noreply.github.com>
2024-03-20 11:53:37 +0100

    Add files via upload

---
+++ BBL/DTO/OrderDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBL.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public List<OrderPositionDTO> OrderPositions { get; set; }
+    }
+}
---
+++ BBL/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using BBL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBL.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<ProductDTO> GetProducts(int page, int pageSize, string filterByName, bool? isActive, string sortBy, bool isDescending);
+        public ProductDTO AddProduct(ProductDTO productDto);
+        public ProductDTO UpdateProduct(int id, ProductDTO productDto);
+        public bool DeleteProduct(int id);
+        bool ActivateProduct(int productId);
+    }
+}
