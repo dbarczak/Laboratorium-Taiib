d390f485b67146aeb7a323ba93fc02a38be48adf
dbarczak <92883028+dbarczak@users.noreply.github.com>
2024-03-26 19:52:13 +0100

    Add files via upload

---
+++ BBL/Interfaces/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿using BBL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBL.Interfaces
+{
+    public interface IBasketService
+	{
+		bool AddToBasket(int userId, int productId, int quantity);
+		bool RemoveFromBasket(int basketItemId);
+		bool ChangeBasketItemQuantity(int basketItemId, int quantity);
+		IEnumerable<BasketItemDTO> GetBasketItems(int userId);
+	}
+}
